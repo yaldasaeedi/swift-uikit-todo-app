4366569ee95738b325bac5d0e09f1e86913792c2
rajman <rajman@rajmans-mac-mini.local>
2023-08-20 10:49:56 +0430

    todo list app - add functionality

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,18 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    //outlet
+    
+
+    @IBOutlet weak var tblList: UITableView!
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var btnDelete: UIButton!
+    @IBOutlet var main: UIView!
+    @IBOutlet weak var txtField: UITextField!
+    var test = ["hi", "hiiii"]
+//function
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tblList.dataSource = self
+        tblList.delegate = self
+        main.backgroundColor = .black
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func addClicked(_ sender: Any) {
+        guard let text = txtField.text, !text.isEmpty else {
+            return // Exit if the text field is empty
+        }
+        
+        test.append(text)
+        tblList.reloadData()
+        txtField.text = ""
+    }
+    
+    func numberOfSections(in tableView:UITableView) -> Int{
+        return 1;
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int{
+        return test.count
+    }
+    //sets the table cells text
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel!.text = test[indexPath.row]
+        return cell
+    }
+    
+    
 }
-
